3bfd650af97535c2ea6eaa78af1d7fb37d654040
Pavel Kogut <kogut.kogut1@mail.ru>
2023-11-04 20:03:53 +0500

    The Accordeon of books section is ready

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -1,1 +1,100 @@
-!function(e){var t={};function n(r){if(t[r])return t[r].exports;var o=t[r]={i:r,l:!1,exports:{}};return e[r].call(o.exports,o,o.exports,n),o.l=!0,o.exports}n.m=e,n.c=t,n.d=function(e,t,r){n.o(e,t)||Object.defineProperty(e,t,{enumerable:!0,get:r})},n.r=function(e){"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},n.t=function(e,t){if(1&t&&(e=n(e)),8&t)return e;if(4&t&&"object"==typeof e&&e&&e.__esModule)return e;var r=Object.create(null);if(n.r(r),Object.defineProperty(r,"default",{enumerable:!0,value:e}),2&t&&"string"!=typeof e)for(var o in e)n.d(r,o,function(t){return e[t]}.bind(null,o));return r},n.n=function(e){var t=e&&e.__esModule?function(){return e.default}:function(){return e};return n.d(t,"a",t),t},n.o=function(e,t){return Object.prototype.hasOwnProperty.call(e,t)},n.p="",n(n.s=0)}([function(e,t){window.addEventListener("DOMContentLoaded",(function(){var e=document.querySelectorAll(".card__title"),t=document.querySelectorAll(".card__genres");function n(e,t){e.forEach((function(e){if(e.textContent.length>t){var n=e.textContent.slice(0,t-2);e.textContent=n+"..."}}))}n(e,24),n(t,37)}))}]);
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/assets/js/app.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/assets/js/app.js":
+/*!******************************!*\
+  !*** ./src/assets/js/app.js ***!
+  \******************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("window.addEventListener('DOMContentLoaded', () => {\r\n    // Находим заголовки карточек и обрезаем если слишком длинное название\r\n    const cardTitles = document.querySelectorAll('.card__title');\r\n    const cardGenres = document.querySelectorAll('.card__genres');\r\n\r\n    function cutLength(strings, maxLength) {\r\n        strings.forEach((item) => {\r\n            if (item.textContent.length > maxLength) {\r\n                let titleValue = item.textContent.slice(0, maxLength - 2);\r\n                item.textContent = titleValue + '...';\r\n            }\r\n        });\r\n    }\r\n\r\n    cutLength(cardTitles, 24);\r\n    cutLength(cardGenres, 37);\r\n1\r\n\r\n    $(function () {\r\n        //обрабатываем клик по блоку с классом trigger\r\n        $('.trigger').on('click', function (e) {\r\n            e.preventDefault();\r\n            //получаем нужные нам данные\r\n            var $this = $(this),\r\n                //получаем всё блоки menu\r\n                container = $this.closest('.menu'),\r\n                //получаем li по которому кликнули\r\n                item = $this.closest('.item'),\r\n                //получаем все другие li\r\n                items = container.find('.item'),\r\n                //выбираем из li активный\r\n                activeItem = items.filter('.active'),\r\n                //выбираем из li по которому кликнули блок контен\r\n                content = item.find('.content'),\r\n                //выбираем у li с классом active блок контент\r\n                activeContent = activeItem.find('.content');\r\n\r\n            //если нет li с классом active по которому кликнули\r\n            if (!item.hasClass('active')) {\r\n                //убираем класс active\r\n                items.removeClass('active');\r\n                //добавляем active кликнутому\r\n                item.addClass('active');\r\n                //у того у кого был active задаём ширину 0\r\n                activeContent.animate({\r\n                    width: '0',\r\n                });\r\n                //кликнутому 294\r\n                content.animate({\r\n                    width: '29.4rem',\r\n                });\r\n                //иначе\r\n            } else {\r\n                item.removeClass('active');\r\n                content.animate({\r\n                    width: '0',\r\n                });\r\n            }\r\n        });\r\n        $('.item-4').click();\r\n    });\r\n    \r\n    \r\n});\r\n\n\n//# sourceURL=webpack:///./src/assets/js/app.js?");
+
+/***/ })
+
+/******/ });
--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,11 +1,10 @@
 window.addEventListener('DOMContentLoaded', () => {
-
     // Находим заголовки карточек и обрезаем если слишком длинное название
     const cardTitles = document.querySelectorAll('.card__title');
     const cardGenres = document.querySelectorAll('.card__genres');
 
     function cutLength(strings, maxLength) {
-        strings.forEach(item => {
+        strings.forEach((item) => {
             if (item.textContent.length > maxLength) {
                 let titleValue = item.textContent.slice(0, maxLength - 2);
                 item.textContent = titleValue + '...';
@@ -15,6 +14,51 @@ window.addEventListener('DOMContentLoaded', () => {
 
     cutLength(cardTitles, 24);
     cutLength(cardGenres, 37);
+1
+
+    $(function () {
+        //обрабатываем клик по блоку с классом trigger
+        $('.trigger').on('click', function (e) {
+            e.preventDefault();
+            //получаем нужные нам данные
+            var $this = $(this),
+                //получаем всё блоки menu
+                container = $this.closest('.menu'),
+                //получаем li по которому кликнули
+                item = $this.closest('.item'),
+                //получаем все другие li
+                items = container.find('.item'),
+                //выбираем из li активный
+                activeItem = items.filter('.active'),
+                //выбираем из li по которому кликнули блок контен
+                content = item.find('.content'),
+                //выбираем у li с классом active блок контент
+                activeContent = activeItem.find('.content');
+
+            //если нет li с классом active по которому кликнули
+            if (!item.hasClass('active')) {
+                //убираем класс active
+                items.removeClass('active');
+                //добавляем active кликнутому
+                item.addClass('active');
+                //у того у кого был active задаём ширину 0
+                activeContent.animate({
+                    width: '0',
+                });
+                //кликнутому 294
+                content.animate({
+                    width: '29.4rem',
+                });
+                //иначе
+            } else {
+                item.removeClass('active');
+                content.animate({
+                    width: '0',
+                });
+            }
+        });
+        $('.item-4').click();
+    });
     
-    }
-)
+    
+});
