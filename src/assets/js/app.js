874cf6fb18617484baf4509598972492e2e764cb
Pavel Kogut <kogut.kogut1@mail.ru>
2023-11-26 15:46:51 +0500

    Pagination

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,16 +1,14 @@
 window.addEventListener('DOMContentLoaded', () => {
-
-
     // Мобильное меню
 
     let menuBtn = document.querySelector('.menu-btn');
     let menu = document.querySelector('.menu');
-    menuBtn.addEventListener('click', function(){
+    menuBtn.addEventListener('click', function () {
         menuBtn.classList.toggle('active');
         menu.classList.toggle('active');
         document.body.classList.toggle('mobile-menu-active');
         window.scrollBy(0, -85);
-    })
+    });
 
     // Находим заголовки карточек и обрезаем если слишком длинное название
     const cardTitles = document.querySelectorAll('.card__title');
@@ -128,4 +126,58 @@ window.addEventListener('DOMContentLoaded', () => {
         autoplay: true,
         autoplayHoverPause: true,
     });
+
+    // Пагинация
+
+    const content = document.querySelector('.content');
+    const itemsPerPage = 14; // set number of items per page
+    let currentPage = 0;
+    const items = Array.from(content.querySelectorAll('.card')).slice(0);
+
+    function showPage(page) {
+        const startIndex = page * itemsPerPage;
+        const endIndex = startIndex + itemsPerPage;
+        items.forEach((item, index) => {
+            item.classList.toggle(
+                'hidden',
+                index < startIndex || index >= endIndex
+            );
+        });
+        updateActiveButtonStates();
+    }
+
+    function createPageButtons() {
+        const totalPages = Math.ceil(items.length / itemsPerPage);
+        const paginationContainer = document.createElement('div');
+        const paginationDiv = document.body.appendChild(paginationContainer);
+        paginationContainer.classList.add('bookspage__pagination');
+
+        // Add page buttons
+        for (let i = 0; i < totalPages; i++) {
+            const pageButton = document.createElement('button');
+            pageButton.textContent = i + 1;
+            pageButton.addEventListener('click', () => {
+                currentPage = i;
+                showPage(currentPage);
+                updateActiveButtonStates();
+            });
+
+            content.parentNode.appendChild(paginationContainer);
+            paginationDiv.appendChild(pageButton);
+        }
+    }
+
+    function updateActiveButtonStates() {
+        const pageButtons = document.querySelectorAll('.bookspage__pagination button');
+        pageButtons.forEach((button, index) => {
+            if (index === currentPage) {
+                button.classList.add('active');
+            } else {
+                button.classList.remove('active');
+            }
+        });
+    }
+
+    createPageButtons(); // Call this function to create the page buttons initially
+    showPage(currentPage);
 });
